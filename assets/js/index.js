1327538150996a3d2f2fe08e21a9e85dd21dbd41
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-05-29 14:59:53 +0100

    add player move downwards

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,17 +11,29 @@ class Player{
 	constructor()
 	{
 		this.position ={x: 100, y: 100};
-		this.width = 100;
-		this.height = 100;
+		this.velocity = {x: 0, y:1};
+		this.width = 30;
+		this.height = 30;
 		this.color = 'red';
 	}
 	draw() {
 		c.fillStyle = this.color;
 		c.fillRect(this.position.x, this.position.y, this.width, this.height);
 	}
+	update() {
+		this.draw();
+		this.position.y += this.velocity.y;
+	}
 }
 
 const player = new Player();
 
-player.draw();
+player.update();
 
+function animate() {
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, canvas.width, canvas.height);
+	player.update();
+}
+
+animate();
