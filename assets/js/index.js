3d734b0a156f9761af96e64d28e9ed273858bf53
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-05-29 14:41:46 +0100

    add player displays to screen

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector('canvas');
+
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+console.log(c);
+
+class Player{
+	constructor()
+	{
+		this.position ={x: 100, y: 100};
+		this.width = 100;
+		this.height = 100;
+		this.color = 'red';
+	}
+	draw() {
+		c.fillStyle = this.color;
+		c.fillRect(this.position.x, this.position.y, this.width, this.height);
+	}
+}
+
+const player = new Player();
+
+player.draw();
+
